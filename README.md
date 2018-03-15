3cebd09fa239721dbf27bc631cc022ec2cedc8f6
sptj <sptj1993@gmail.com>
2018-03-15 16:15:44 +0800

    # In WebCamCtrl -add functions to calc the direction and the speed for crtl command

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AutoCamera
+this repo is designed for win64
+## To use this repo of code you need to add the directory of "HC" to your environment, PATH 
--- WebCamCtrl.py
+++ WebCamCtrl.py
@@ -1,50 +1,32 @@
-import os
-import ctypes, ctypes.wintypes
-import HCNetSdkTypes
-import time
-try:
-    dll = os.path.join(os.path.dirname(os.path.abspath(__file__)), "./HC/HCNetSDK.dll") # the directory with the .py file
-    dll2 = os.path.join("./HC/HCNetSDK.dll") # currnet directory
-    if not os.path.isfile(dll):
-        if not os.path.isfile(dll2):
-            raise Exception("File not found: %s or %s" % (dll,dll2))
-        else:
-            hsdk = ctypes.WinDLL(dll2) # STEP-1
-            print("dll2 loaded")
-    else:
-        hsdk = ctypes.WinDLL(dll) # STEP-1
-        print("dll loaded")
-        hsdk.NET_DVR_Init()
-        hsdk.NET_DVR_SetConnectTime(2000, 1)
-        hsdk.NET_DVR_SetReconnect(10000, True)
-        struLoginInfo = HCNetSdkTypes.NET_DVR_USER_LOGIN_INFO()
-        struLoginInfo.bUseAsynLogin = 0
-        struLoginInfo.sDeviceAddress = bytes("192.168.1.64",encoding="utf8")
-        struLoginInfo.wPort = 8000
-        struLoginInfo.sUserName = bytes("admin",encoding="utf8")
-        struLoginInfo.sPassword = bytes("jishukaifa3432",encoding="utf8")
-        struDeviceInfoV40 = HCNetSdkTypes.NET_DVR_DEVICEINFO_V40()
+# (x1,y1)---------------------(x2,y1)
+#    |                           |
+#    |                           |
+#    |                           |
+#    0------------------------(x2,y2)
+# the default size of video frame is 640*480
+import math
+def Angle2Direction(angle):
+    abs_angle=abs(angle)
+    direction=round(angle/45)
+    return int(math.copysign(direction,angle))
+def Position2DirectionPosition(x,y):
+    range=math.sqrt(x*x+y*y)    #note that rangle_max=800
+    angle = math.atan2(y, x)/math.pi*180
+    direction=Angle2Direction(angle)
+    speed=math.ceil(range/100)
+    if(speed>7):
+        speed=7
+    if(abs(direction)==2):
+        speed=round(speed/480.0*640.0)
+    return (direction,speed)
+if __name__ =="__main__":
+    # lst = [1, 1, 0, 2, 0, 0, 8, 3, 0, 2, 5, 0, 2, 6]
+    # lst = [x for x in lst if x != 0]
+    # print(lst)
+    # TransPosition2Command(1,-1)
+    print(Position2DirectionPosition(-10,1))
 
 
-
-        lUserID=ctypes.c_long(hsdk.NET_DVR_Login_V40(ctypes.byref(struLoginInfo), ctypes.byref(struDeviceInfoV40)))
-
-        hsdk.NET_DVR_PTZControlWithSpeed_Other(lUserID,ctypes.c_long(1), ctypes.wintypes.DWORD(27),
-                                               ctypes.wintypes.DWORD(0),ctypes.wintypes.DWORD(7))
-        time.sleep(1)
-        hsdk.NET_DVR_PTZControlWithSpeed_Other(lUserID, ctypes.c_long(1), ctypes.wintypes.DWORD(27),
-                                               ctypes.wintypes.DWORD(1), ctypes.wintypes.DWORD(7))
-        hsdk.NET_DVR_Logout(lUserID)
-        hsdk.NET_DVR_Cleanup()
-        print(hsdk.NET_DVR_GetLastError())
-
-
-
-
-except Exception:
-    raise Exception
-# version = "%.8x" % hsdk.PlayM4_GetSdkVersion()
-# print (version, " <- PlayM4_GetSdkVersion()")
 
 
 
